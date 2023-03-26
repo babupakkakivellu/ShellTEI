4ea7bd960a7a9d42b882dc7747463f81b99dbccc
Mohanish <127211714+TEIMohanish@users.noreply.github.com>
2023-03-26 13:05:28 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
 api_id = 16546151
 api_hash = "41f98a52cd38d06cb64d62472d8d91e5"
-bot_token = "5106638655:AAFM3CMXf1MBbbkmDIelBMo5WHt9qI0jwj0"
+bot_token = "5568498977:AAFLmxBr-b4EASZ9JTTLVlrslDVis0zOZ3U"
