799a7cdba8db59a8968cac7aa3c9681544f7c5ec
Mohanish <127211714+TEIMohanish@users.noreply.github.com>
2023-03-26 14:41:09 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,13 +14,13 @@ tony = Client(name="My Tony", api_id=vars.api_id, api_hash=vars.api_hash, bot_to
 
 @tony.on_message(filters.command('start') & filters.private)
 def start(client, message):
-    message.reply_text(text="welcome to ANToNi rip bot")
-    print("Welcome to tony bot")
+    message.reply_text(text="welcome to Toon Encodes rip bot")
+    print("Welcome to Toon Encodes bot")
 
 @tony.on_message(filters.command('help') & filters.private)
 def help(client, message):
     if help:
-       message.reply_text(text="this bot just for shell, made by @tont9848")
+       message.reply_text(text="this bot just for shell, made by @ToonEncodes")
     else:
        message.reply_text(text="No helps")
 
