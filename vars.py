be1551adc13e541030345d93c2720115f838704f
babupakkakivellu <149465835+babupakkakivellu@users.noreply.github.com>
2024-06-16 22:03:52 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,3 +1,3 @@
 api_id = 24118505
 api_hash = "b16a89baa3aa500b1d8339a0f0132f59"
-bot_token = "5106638655:AAFM3CMXf1MBbbkmDIelBMo5WHt9qI0jwj0"
+bot_token = "6880015232:AAGgDfGzbhqVrrLM_WoQtMe5hdDHW4HYVIA"
